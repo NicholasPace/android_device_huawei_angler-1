ec448892763daa8c2b02bdd3dd1363e02bee3638
Nicholas Pace <nicholaspace97@yahoo.ca>
2016-05-14 10:01:27 -0500

    Update and rename cm.mk to xosp.mk
    xosp bringup

--- xosp.mk
+++ xosp.mk
@@ -2,11 +2,12 @@
 TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1440
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+#inherit telephony stuff
+$(call inherit-product, vendor/xosp/config/telephony.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+# Inherit some common XOSP stuff.
+$(call inherit-product, vendor/xosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/xosp/config/xosp.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/huawei/angler/aosp_angler.mk)
@@ -14,11 +15,13 @@ $(call inherit-product, device/huawei/angler/aosp_angler.mk)
 DEVICE_PACKAGE_OVERLAYS += device/huawei/angler/overlay-cm
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_angler
+PRODUCT_NAME := xosp_angler
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 6P
 
 TARGET_VENDOR := huawei
+
+IS_ARM64 := TRUE
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
